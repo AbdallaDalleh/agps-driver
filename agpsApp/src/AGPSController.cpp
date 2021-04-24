b8cd3190ddc89965ac6d8bc1ad21dbb930917257
Abdalla Al-Dalleh <cpe.3bdalla@gmail.com>
2021-04-24 15:47:58 +0300

    Added asyn parameters strings. Implemented readInt32 interface.
    Files:
      1. agpsApp/src/AGPSController.cpp

--- agpsApp/src/AGPSController.cpp
+++ agpsApp/src/AGPSController.cpp
@@ -10,14 +10,25 @@ using std::endl;
 
 #include "agps.h"
 
+#define I_PARAMETER "i_parameter"
+#define F_PARAMETER "f_parameter"
+#define I_REGISTER  "i_register"
+#define F_REGISTER  "f_register"
+
 class AGPSController : public asynPortDriver
 {
 public:
     AGPSController(const char* port_name, const char* asyn_name);
-    // virtual asynStatus readInt32(asynUser *pasynUser, epicsInt32 *value);
+    virtual asynStatus readInt32(asynUser *pasynUser, epicsInt32 *value);
     // virtual asynStatus readFloat64(asynUser *pasynUser, epicsFloat64 *value);
     // virtual asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
     // virtual asynStatus writeFloat64(asynUser *pasynUser, epicsFloat64 value);
+
+protected:
+    int index_i_parameter;
+    int index_f_parameter;
+    int index_i_register;
+    int index_f_register;
 
 private:
     asynUser* asyn_user;
@@ -26,10 +37,10 @@ private:
 
 AGPSController::AGPSController(const char* port_name, const char* name)
     : asynPortDriver(port_name,
-                    1,
+                    255,
                     asynInt32Mask | asynFloat64Mask,
                     0,
-                    ASYN_CANBLOCK,
+                    ASYN_CANBLOCK | ASYN_MULTIDEVICE,
                     1,
                     0, 0)
 {
@@ -39,6 +50,69 @@ AGPSController::AGPSController(const char* port_name, const char* name)
         // TODO: Use asynPrint
         fprintf(stderr, "Could not connect to port %s\n", name);
         return;
+    }
+
+    createParam(F_PARAMETER, asynParamFloat64, &index_f_parameter);
+    createParam(I_PARAMETER, asynParamInt32,   &index_i_parameter);
+    createParam(F_REGISTER,  asynParamFloat64, &index_f_register);
+    createParam(I_REGISTER,  asynParamInt32,   &index_i_register);
+}
+
+asynStatus AGPSController::readInt32(asynUser *pasynUser, epicsInt32 *value)
+{
+    int address;
+    int function = pasynUser->reason;
+    int status;
+    uint32_t data;
+
+    getAddress(pasynUser, &address);
+    if(function == index_i_register)
+    {
+        // These are addresses for integer registers in the register map.
+        if((address >= 0 && address <= 7) || address == 48 || address == 50)
+        {
+            status = write_read(COMMAND_READ_REGISTER, address, &data);
+            if(status != asynSuccess)
+            {
+                cout << "Read integer register failed" << endl;
+                return (asynStatus) status;
+            }
+
+            // setIntegerParam(function, data);
+            *value = data;
+            return asynSuccess;
+        }
+        else
+        {
+            cout << "Invalid integer register address" << endl;
+            return asynError;
+        }
+    }
+    else if(function == index_i_parameter)
+    {
+        if(address == 34 || (address >= 36 && address <= 59))
+        {
+            status = write_read(COMMAND_READ_PARAMETER, address, &data);
+            if(status != asynSuccess)
+            {
+                cout << "Read integer parameter failed" << endl;
+                return (asynStatus) status;
+            }
+
+            // setIntegerParam(function, value);
+            *value = data;
+            return asynSuccess;
+        }
+        else
+        {
+            cout << "Invalid integer parameter address" << endl;
+            return asynError;
+        }
+    }
+    else
+    {
+        cout << "Unknown readInt32 function: " << function << endl;
+        return asynError;
     }
 }
 
